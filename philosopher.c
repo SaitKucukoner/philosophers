f45df0d4a716a8eaf1acf5612eaf0faba98a2358
SaitKucukoner <saitkucukonerr@gmail.com>
2026-06-04 11:59:48 +0300

    philosopher.c and philosopher.c have fixed

--- philosopher.c
+++ philosopher.c
@@ -1,34 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosopher.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skucukon <skucukon@student.42istanbul.com.tr>  #+#      +:++#+       */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026-06-04 08:53:14 by skucukon          #+#    #+#             */
+/*   Updated: 2026-06-04 08:53:14 by skucukon         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philo.h"
 
-int     is_philo_ready(t_philo *philo)
+int	is_philo_ready(t_philo *philo)
 {
-    pthread_mutex_lock(&philo->data->mutex_thread_start);
-    pthread_mutex_unlock(&philo->data->mutex_thread_start);
-    if (!philo->data->threads_ready_to_start)
-        return (1);
-    return (0);
+	pthread_mutex_lock(&philo->data->mutex_thread_start);
+	pthread_mutex_unlock(&philo->data->mutex_thread_start);
+	if (!philo->data->threads_ready_to_start)
+		return (1);
+	return (0);
 }
 
-void    *philosopher_life(void *arg)
+void	*philosopher_life(void *arg)
 {
-    t_philo *philo;
+	t_philo	*philo;
 
-    philo = arg;
-    if (is_philo_ready(philo))
-        return (NULL);
-    while (!is_sim_end(philo->data))
-    {
-        if (take_up_the_forks(philo))
-            continue;
-        if (eat(philo))
-        {
-            put_down_the_forks(philo);
-            continue;
-        }
-        put_down_the_forks(philo);
-        sleep_and_think(philo);
-        if (philo->data->number_of_philosophers % 2 != 0)
-            ft_usleep(philo->data->time_to_eat / 10, philo->data);
-    }
-    return (NULL);
+	philo = arg;
+	if (is_philo_ready(philo))
+		return (NULL);
+	while (!is_sim_end(philo->data))
+	{
+		if (take_up_the_forks(philo))
+			continue ;
+		if (eat(philo))
+		{
+			put_down_the_forks(philo);
+			continue ;
+		}
+		put_down_the_forks(philo);
+		sleep_and_think(philo);
+		if (philo->data->number_of_philosophers % 2 != 0)
+			ft_usleep(philo->data->time_to_eat / 10, philo->data);
+	}
+	return (NULL);
 }
--- philosopher_utils.c
+++ philosopher_utils.c
@@ -1,62 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosopher_utils.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skucukon <skucukon@student.42istanbul.com.tr>  #+#      +:++#+       */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026-06-04 08:55:16 by skucukon          #+#    #+#             */
+/*   Updated: 2026-06-04 08:55:16 by skucukon         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philo.h"
 
-int single_philo_case(t_philo *philo)
+int	single_philo_case(t_philo *philo)
 {
-    pthread_mutex_lock(philo->left_fork);
-    print_philosopher_status(philo, FORK);
-    ft_usleep(philo->data->time_to_die + 10, philo->data);
-    pthread_mutex_unlock(philo->left_fork);
-    return (1);
+	pthread_mutex_lock(philo->left_fork);
+	print_philosopher_status(philo, FORK);
+	ft_usleep(philo->data->time_to_die + 10, philo->data);
+	pthread_mutex_unlock(philo->left_fork);
+	return (1);
 }
 
-int take_up_the_forks(t_philo *philo)
+int	take_up_the_forks(t_philo *philo)
 {
-    if (!is_sim_end(philo->data))
-    {
-        if (philo->data->number_of_philosophers == 1)
-            return (single_philo_case(philo));
-        else if (philo->id % 2)
-        {
-            pthread_mutex_lock(philo->right_fork);
-            print_philosopher_status(philo, FORK);
-            pthread_mutex_lock(philo->left_fork);
-            print_philosopher_status(philo, FORK);
-        }
-        else
-        {
-            pthread_mutex_lock(philo->left_fork);
-            print_philosopher_status(philo, FORK);
-            pthread_mutex_lock(philo->right_fork);
-            print_philosopher_status(philo, FORK);
-        }
-        return (0);
-    }
-    return (1);
+	if (!is_sim_end(philo->data))
+	{
+		if (philo->data->number_of_philosophers == 1)
+			return (single_philo_case(philo));
+		else if (philo->id % 2)
+		{
+			pthread_mutex_lock(philo->right_fork);
+			print_philosopher_status(philo, FORK);
+			pthread_mutex_lock(philo->left_fork);
+			print_philosopher_status(philo, FORK);
+		}
+		else
+		{
+			pthread_mutex_lock(philo->left_fork);
+			print_philosopher_status(philo, FORK);
+			pthread_mutex_lock(philo->right_fork);
+			print_philosopher_status(philo, FORK);
+		}
+		return (0);
+	}
+	return (1);
 }
 
-void    put_down_the_forks(t_philo *philo)
+void	put_down_the_forks(t_philo *philo)
 {
-    pthread_mutex_unlock(philo->right_fork);
-    pthread_mutex_unlock(philo->left_fork);
-}
-int eat(t_philo *philo)
-{
-    if (!is_sim_end(philo->data))
-    {
-        print_philosopher_status(philo, EATING);
-        pthread_mutex_lock(&philo->mutex_meal);
-        philo->number_of_eaten_meals++;
-        philo->time_of_last_eaten_meal = get_time_ms();
-        pthread_mutex_unlock(&philo->mutex_meal);
-        ft_usleep(philo->data->time_to_eat, philo->data);
-        return (0);
-    }
-    return (1);
+	pthread_mutex_unlock(philo->right_fork);
+	pthread_mutex_unlock(philo->left_fork);
 }
 
-void    sleep_and_think(t_philo *philo)
+int	eat(t_philo *philo)
 {
-    print_philosopher_status(philo, SLEEPING);
-    ft_usleep(philo->data->time_to_sleep, philo->data);
-    print_philosopher_status(philo, THINKING);
+	if (!is_sim_end(philo->data))
+	{
+		print_philosopher_status(philo, EATING);
+		pthread_mutex_lock(&philo->mutex_meal);
+		philo->number_of_eaten_meals++;
+		philo->time_of_last_eaten_meal = get_time_ms();
+		pthread_mutex_unlock(&philo->mutex_meal);
+		ft_usleep(philo->data->time_to_eat, philo->data);
+		return (0);
+	}
+	return (1);
+}
+
+void	sleep_and_think(t_philo *philo)
+{
+	print_philosopher_status(philo, SLEEPING);
+	ft_usleep(philo->data->time_to_sleep, philo->data);
+	print_philosopher_status(philo, THINKING);
 }
